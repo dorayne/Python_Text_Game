6413693c1b9ab1b4ec15a27dd1b73abb4de8f957
Dorayne McKern <dorayne@gmail.com>
2016-06-11 21:26:25 -0700

    function outlines
    Outline of functions needed to get through the dungeon

---
+++ mythical_dungeon.py
@@ -0,0 +1,49 @@
+# http://learnpythonthehardway.org/book/ex36.html
+
+from sys import exit
+
+def pack_storage():
+    
+def pack_list():
+    
+def death():
+    
+def starting_point():
+    
+def unicorn():
+    
+def hippogriff():
+    
+def minotaur():
+    
+def manticore():
+    
+def centaur():
+    
+def sphinx():
+    
+def chimera():
+    
+def medusa():
+    
+def circe():
+    
+def harpies():
+    
+def phoenix():
+    
+def pegasus():
+    
+def cerberus():
+    
+def sirens():
+    
+def boat():
+    
+def hydra():
+    
+def scylla_charybdis():
+    
+def griffin():
+    
+def treasure()
